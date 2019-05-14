6ec3f1b5ef17e88851a1acdce72757dc892987e2
jamesandere <jamesandere56@gmail.com>
2019-05-14 11:58:18 +0300

    Use adapters to convert array to views

--- app/src/main/java/com/andere/onthelaw/LawAdapter.java
+++ app/src/main/java/com/andere/onthelaw/LawAdapter.java
@@ -1,28 +1,28 @@
-//package com.andere.onthelaw;
-//
-//import android.content.Context;
-//import android.widget.ArrayAdapter;
-//
-//public class LawAdapter extends ArrayAdapter {
-//    private Context mContext;
-//    private String[] mcases;
-//    private String[] mparties;
-//
-//    public LawAdapter(Context mContext, int resource, String[] mcases, String[] mparties) {
-//        super(mContext, resource);
-//        this.mContext = mContext;
-//        this.mcases = mcases;
-//        this.mparties = mparties;
-//    }
-//    @Override
-//    public Object getItem(int position) {
-//        String cases = mcases[position];
-//        String parties = mparties[position];
-//        return String.format("%s \nCase: %s", cases, parties);
-//    }
-//
-//    @Override
-//    public int getCount() {
-//        return mcases.length;
-//    }
-//}
+package com.andere.onthelaw;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class LawAdapter extends ArrayAdapter {
+    private Context mContext;
+    private String[] mcases;
+    private String[] mparties;
+
+    public LawAdapter(Context mContext, int resource, String[] mcases, String[] mparties) {
+        super(mContext, resource);
+        this.mContext = mContext;
+        this.mcases = mcases;
+        this.mparties = mparties;
+    }
+    @Override
+    public Object getItem(int position) {
+        String cases = mcases[position];
+        String parties = mparties[position];
+        return String.format("%s \nCase: %s", cases, parties);
+    }
+
+    @Override
+    public int getCount() {
+        return mcases.length;
+    }
+}
