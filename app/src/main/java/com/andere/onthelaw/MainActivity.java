17a5a8033e7930448736245872df277366b1055d
jamesandere <jamesandere56@gmail.com>
2019-05-14 11:49:23 +0300

    Implement butterknife

--- app/src/main/java/com/andere/onthelaw/MainActivity.java
+++ app/src/main/java/com/andere/onthelaw/MainActivity.java
@@ -13,26 +13,26 @@ import android.widget.Toast;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 //
-//public class MainActivity extends AppCompatActivity {
-//  @BindView(R.id.findbutton) Button mFindcaseButton;
-//  @BindView(R.id.appNameTextView) TextView mAppNameTextView;
-//  @BindView(R.id.caseInput) EditText mcase;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        ButterKnife.bind(this);
+public class MainActivity extends AppCompatActivity {
+  @BindView(R.id.findbutton) Button mFindcaseButton;
+  @BindView(R.id.appNameTextView) TextView mAppNameTextView;
+  @BindView(R.id.caseInput) EditText mcase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 //
 //        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/font1.ttf");
 //
         mFindcaseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String case = mcase.getText().toString();
+                String casee = mcase.getText().toString();
                 Toast.makeText(MainActivity.this , "searching .." ,Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(MainActivity.this , CasesActivity.class);
-                intent.putExtra("case", case);
+                intent.putExtra("casee", casee);
 
                 startActivity(intent);
 
