a0fc1f5a719a9148cb1ad333b9444010094f4799
jamesandere <jamesandere56@gmail.com>
2019-05-14 11:44:30 +0300

    Create intent to help navigate through activities

--- app/src/main/java/com/andere/onthelaw/MainActivity.java
+++ app/src/main/java/com/andere/onthelaw/MainActivity.java
@@ -1,17 +1,17 @@
-//package com.andere.onthelaw;
-//
-//import android.content.Intent;
-//import android.graphics.Typeface;
-//import android.support.v7.app.AppCompatActivity;
-//import android.os.Bundle;
-//import android.view.View;
-//import android.widget.Button;
-//import android.widget.EditText;
-//import android.widget.TextView;
-//import android.widget.Toast;
-//
-//import butterknife.BindView;
-//import butterknife.ButterKnife;
+package com.andere.onthelaw;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 //
 //public class MainActivity extends AppCompatActivity {
 //  @BindView(R.id.findbutton) Button mFindcaseButton;
@@ -26,18 +26,18 @@
 //
 //        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/font1.ttf");
 //
-//        mFindcaseButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                String year = mcase.getText().toString();
+        mFindcaseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String case = mcase.getText().toString();
 //                Toast.makeText(MainActivity.this , "searching .." ,Toast.LENGTH_LONG).show();
-//                Intent intent = new Intent(MainActivity.this , CasesActivity.class);
-//                intent.putExtra("year", year);
-//
-//                startActivity(intent);
-//
-//            }
-//        });
+                Intent intent = new Intent(MainActivity.this , CasesActivity.class);
+                intent.putExtra("case", case);
+
+                startActivity(intent);
+
+            }
+        });
 //    }
 //
 //}
